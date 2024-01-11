1ef1c723d37adc4751004017fe2631a0bc218c3e
apolline-diaz <apolline.diaz@gmail.com>
2024-01-11 16:38:21 +0100

    Actualisation aléatoire du fond d'écran

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+// Actualisation fond d'écran
+
+function changeimg() {
+
+    const images = [
+        "chihiro008.jpg",
+        "ged014.jpg",
+        "karigurashi018.jpg",
+        "kokurikozaka049.jpg",
+        "ponyo032.jpg"
+    ];
+    
+    const randomIndex = Math.floor(Math.random() * images.length);    
+    document.getElementById("container").style.backgroundImage = `url('images/${images[randomIndex]}')`;
+}
+
+document.addEventListener("DOMContentLoaded", changeimg);
+
+
+
+/*Actualisation fond d'écran
+
+function changeimg(){
+    
+    let images = ["url('images/chihiro008.jpg')", "url('images/ged014.jpg')","url('images/karigurashi018.jpg')","url('images/kokurikozaka049.jpg')","url('images/ponyo032.jpg')"];
+
+    let randomimg = Math.floor(Math.random() * images.length) +0;
+
+    document.getElementById("container").style.backgroundImage=images[randomimg];
+
+    };*/
+
+//Automatisation Date
+
 const date = document.getElementById("date");
 const day = document.getElementById("day");
 const month = document.getElementById("month");
@@ -12,6 +46,8 @@ date.innerHTML = today.getDate();
 day.innerHTML = weekDays[today.getDay()];
 month.innerHTML = allMonths[today.getMonth()];
 year.innerHTML = today.getFullYear();
+
+//Automatisation Horaire
 
 let hrs = document.getElementById("hrs");
 let min = document.getElementById("min");
