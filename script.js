4259e068dfb64580cba6c0d122bb50e8c465805b
apolline-diaz <apolline.diaz@gmail.com>
2024-01-15 12:37:21 +0100

    Météo

--- script.js
+++ script.js
@@ -10,6 +10,17 @@ function changeimg() {
         "karigurashi018.jpg",
         "kokurikozaka049.jpg",
         "ponyo032.jpg",
+        "kaze1.jpg",
+        "mononoke010.jpg",
+        "mononoke023.jpg",
+        "nausicaa002.jpg",
+        "nausicaa046.jpg",
+        "tanuki010.jpg",
+        "tanuki028.jpg",
+        "tanuki035.jpg",
+        "totoro024.jpg",
+        "totoro025.jpg",
+        "totoro037.jpg"
     ];
 
     
@@ -30,20 +41,6 @@ function changeimg() {
 
 document.addEventListener("DOMContentLoaded", changeimg);
 document.getElementById("changeBackground").addEventListener("click", changeimg);
-
-
-
-/*Actualisation fond d'écran
-
-function changeimg(){
-    
-    let images = ["url('images/chihiro008.jpg')", "url('images/ged014.jpg')","url('images/karigurashi018.jpg')","url('images/kokurikozaka049.jpg')","url('images/ponyo032.jpg')"];
-
-    let randomimg = Math.floor(Math.random() * images.length) +0;
-
-    document.getElementById("container").style.backgroundImage=images[randomimg];
-
-    };*/
 
 //Automatisation Date
 
@@ -77,4 +74,55 @@ setInterval(()=>{
 
 },1000)
 
+// Météo 
 
+    const apiKey="205fe95868c98ce3ff001e304e78b5cf";
+    const apiUrl= "https://api.openweathermap.org/data/2.5/weather?units=metric&q="; //unités = métrique
+    
+    const searchBox = document.querySelector(".search input");
+    const searchBtn = document.querySelector(".search button");
+    const weatherIcon = document.querySelector(".weather-icon");
+
+    async function checkWeather(city){
+        const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+ 
+        if(response.status == 404){
+            document.querySelector(".error").style.display="block";
+            document.querySelector(".weather").style.display="none";
+        }else{
+
+            var data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "images/clouds.png";         
+    }
+    else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "images/clear.png";         
+    }
+    else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "images/rain.png";         
+    }
+    else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "images/drizzle.png";         
+    }
+    else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "images/mist.png";         
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display="none";
+
+        }
+        
+    }
+
+    searchBtn.addEventListener("click", ()=> {
+        checkWeather(searchBox.value);
+    })
+
+    checkWeather();
